c2b4dd98d829f2e11d851b609df7d463197e00c9
Alex Fourmy <alexfourmy@gmail.com>
2021-11-19 09:23:47 +0100

    Added logic to retrieve all posts when on a user profile

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -5,22 +5,25 @@ import Share from '../share/Share'
 import './feed.css'
 import axios from 'axios'
 
-export default function Feed() {
+export default function Feed({ username }) {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get('posts/timeline/61951e25275a73bfed029041')
+      const res = username
+        ? await axios.get('/posts/profile/' + username)
+        : await axios.get('posts/timeline/61951e25275a73bfed029041')
       setPosts(res.data)
     }
     fetchPosts()
-  }, [])
+  }, [username])
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
         {posts.map(p => (
-          <Post key={p.id} post={p} />
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
