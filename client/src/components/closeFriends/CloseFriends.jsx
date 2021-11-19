c20bf7f8aa3b10bce7121b85565fc229409f9c87
Alex Fourmy <alexfourmy@gmail.com>
2021-11-19 19:56:13 +0100

    Added a new css class

--- client/src/components/closeFriends/CloseFriends.jsx
+++ client/src/components/closeFriends/CloseFriends.jsx
@@ -5,7 +5,7 @@ export default function CloseFriends({ user }) {
   return (
     <li className="sidebarFriend">
       <img src={PF + user.profilePicture} alt="" className="sidebarFriendImg" />
-      <span className="sidebarFiendName">{user.username}</span>
+      <span className="sidebarFriendName">{user.username}</span>
     </li>
   )
 }
