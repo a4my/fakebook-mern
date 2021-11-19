53542881c7b4015d429766ec0e7a1817a21e04b5
Alex Fourmy <alexfourmy@gmail.com>
2021-11-19 19:57:58 +0100

    Sorted posts by date

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,27 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import Post from '../post/Post'
 import Share from '../share/Share'
 // import { Posts } from '../../dummyData'
 import './feed.css'
 import axios from 'axios'
+import { AuthContext } from '../../context/AuthContext'
 
 export default function Feed({ username }) {
   const [posts, setPosts] = useState([])
+  const { user } = useContext(AuthContext)
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = username
         ? await axios.get('/posts/profile/' + username)
-        : await axios.get('posts/timeline/61951e25275a73bfed029041')
-      setPosts(res.data)
+        : await axios.get('posts/timeline/' + user._id)
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt)
+        })
+      )
     }
     fetchPosts()
-  }, [username])
+  }, [username, user._id])
 
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Share />
+        {(!username || username === user.username) && <Share />}
         {posts.map(p => (
           <Post key={p._id} post={p} />
         ))}
