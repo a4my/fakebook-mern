68a34769504976b865945c8820443e6c3bca7a77
Alex Fourmy <alexfourmy@gmail.com>
2021-11-19 09:25:15 +0100

    worked on retrieving all information of a single post, ie post date, username, post picture and text

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,20 +3,22 @@ import { MoreVert } from '@material-ui/icons'
 // import { Users } from '../../dummyData'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { format } from 'timeago.js'
+import { Link } from 'react-router-dom'
 
 export default function Post({ post }) {
-  const [like, setLike] = useState(post.like)
+  const [like, setLike] = useState(post.likes.length)
   const [isLiked, setIsLiked] = useState(false)
   const [user, setUser] = useState({})
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`users/${post.userId}`)
+      const res = await axios.get(`/users?userId=${post.userId}`)
       setUser(res.data)
     }
     fetchUser()
-  }, [])
+  }, [post.userId])
 
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1)
@@ -27,9 +29,15 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src={user.profilePicture} alt="" />
+            <Link to={`profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={user.profilePicture || PF + '/noavatar.png'}
+                alt=""
+              />
+            </Link>
             <span className="postUsername">{user.username}</span>
-            <span className="postDate">{post.date}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -37,7 +45,7 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF + post.photo} alt="" />
+          <img className="postImg" src={PF + post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
@@ -53,10 +61,10 @@ export default function Post({ post }) {
               onClick={likeHandler}
               alt=""
             />
-            <span className="postLikeCounter">{like}</span>
+            <span className="postLikeCounter">{like} people liked it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">{post.comment}</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
