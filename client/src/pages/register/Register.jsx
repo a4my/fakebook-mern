735a430e51931d260669e0105ff984d65556d1d0
Alex Fourmy <alexfourmy@gmail.com>
2021-11-22 14:13:00 +0100

    Added a Link to the login button

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useRef } from 'react'
 import './register.css'
 import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   const username = useRef()
@@ -22,7 +23,7 @@ export default function Register() {
       }
       try {
         await axios.post('/auth/register', user)
-        history.push('/login')
+        history('/login')
       } catch (err) {
         console.log(err)
       }
@@ -71,7 +72,9 @@ export default function Register() {
             <button className="loginButton" type="submit">
               Sign Up
             </button>
-            <button className="loginRegisterButton">Log into Account</button>
+            <Link to={'/Login'}>
+              <button className="loginRegisterButton">Log into Account</button>
+            </Link>
           </form>
         </div>
       </div>
