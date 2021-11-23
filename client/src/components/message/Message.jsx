ba18b728ed5db1b0abe3561500942f314d1aa297
Alex Fourmy <alexfourmy@gmail.com>
2021-11-23 17:30:45 +0100

    Added dynamic info to messages

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,6 +1,7 @@
 import './message.css'
+import { format } from 'timeago.js'
 
-export default function Message({ own }) {
+export default function Message({ message, own }) {
   return (
     <div className={own ? 'message own' : 'message'}>
       <div className="messageTop">
@@ -10,13 +11,9 @@ export default function Message({ own }) {
           alt=""
           className="conversationImg"
         />
-        <p className="messageText">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-          perspiciatis provident placeat asperiores aliquid autem nisi alias
-          odio omnis expedita.
-        </p>
+        <p className="messageText">{message.text}</p>
       </div>
-      <div className="messageBottom">1 hour ago</div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
     </div>
   )
 }
