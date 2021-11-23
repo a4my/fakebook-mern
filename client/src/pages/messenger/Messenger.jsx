5daf41866caa94af999eaf4fa2cc00316ace3618
Alex Fourmy <alexfourmy@gmail.com>
2021-11-23 17:28:03 +0100

    Added the conversation message and added logic on displaying conversation

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -3,8 +3,69 @@ import Topbar from '../../components/topbar/Topbar'
 import Conversation from '../../components/conversation/Conversation'
 import Message from '../../components/message/Message'
 import ChatOnline from '../../components/chatOnline/ChatOnline'
+import { useContext, useEffect, useRef, useState } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import axios from 'axios'
+import { io } from 'socket.io-client'
 
 export default function Messenger() {
+  const [conversations, setConversations] = useState([])
+  const [currentChat, setCurrentChat] = useState(null)
+  const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+  const socket = useRef(io('ws://localhost:8900'))
+  const { user } = useContext(AuthContext)
+  const scrollRef = useRef()
+
+  useEffect(() => {
+    socket.current.emit('addUser', user._id)
+  }, [user])
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get('/conversations/' + user._id)
+        setConversations(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getConversations()
+  }, [user._id])
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get('/messages/' + currentChat?._id)
+        setMessages(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getMessages()
+  }, [currentChat])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const message = {
+      sender: user._id,
+      text: newMessage,
+      conversationId: currentChat._id
+    }
+
+    try {
+      const res = await axios.post('/messages', message)
+      setMessages([...messages, res.data])
+      setNewMessage('')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ bahavior: 'smooth' })
+  }, [messages])
+
   return (
     <>
       <Topbar />
@@ -16,33 +77,41 @@ export default function Messenger() {
               type="text"
               placeholder="Search for friends"
             />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map(c => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} currentUser={user} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="chatBox">
           <div className="chatBoxWrapper">
-            <div className="chatBoxTop">
-              <Message />
-              <Message own={true} />
-              <Message />
-              <Message />
-              <Message own={true} />
-              <Message />
-              <Message />
-              <Message own={true} />
-              <Message />
-            </div>
-            <div className="chatBoxBottom">
-              <textarea
-                className="chatMessageInput"
-                placeholder="Write something..."
-              ></textarea>
-              <button className="chatSubmitButton">Submit</button>
-            </div>
+            {currentChat ? (
+              <>
+                <div className="chatBoxTop">
+                  {messages.map(m => (
+                    <div ref={scrollRef}>
+                      <Message message={m} own={m.sender === user._id} />
+                    </div>
+                  ))}
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="Write something..."
+                    onChange={e => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button className="chatSubmitButton" onClick={handleSubmit}>
+                    Send
+                  </button>
+                </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                Open a conversation to start a chat.
+              </span>
+            )}
           </div>
         </div>
         <div className="chatOnline">
