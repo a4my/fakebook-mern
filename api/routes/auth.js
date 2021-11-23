8db85b0325e3700b4024a329fc3fec7c67b0914f
Alex Fourmy <alexfourmy@gmail.com>
2021-11-23 14:45:27 +0100

    Implemented authentification with Express and Bcrypt

---
+++ api/routes/auth.js
@@ -0,0 +1,44 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+// REGISTER
+router.post('/register', async (req, res) => {
+  try {
+    // Generate a hashed password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    // Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
+    // Save new user and respond
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    // Retrieve email user
+    const user = await User.findOne({ email: req.body.email })
+    !user && res.status(400).json('User not found!')
+
+    // Retrieve password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json('Incorrect password!')
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
