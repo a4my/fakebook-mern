7d16d05c799ddddc06c7feb7cc11b8fe2c950eb2
Alex Fourmy <alexfourmy@gmail.com>
2021-12-01 17:28:52 +0100

    Added a Link to the chat page

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -38,8 +38,10 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
+            <Link to="/messenger" className="topbarLinks">
+              <Chat />
+              <span className="topbarIconBadge">2</span>
+            </Link>
           </div>
           <div className="topbarIconItem">
             <Notifications />
