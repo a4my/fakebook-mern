4ea4386ca43602cfd303adb18cfbf947beef2381
Alex Fourmy <alexfourmy@gmail.com>
2021-12-01 17:23:51 +0100

    finished working on the right side of the chat page and got to make online friends show up in the list of online users

--- client/src/components/chatOnline/ChatOnline.jsx
+++ client/src/components/chatOnline/ChatOnline.jsx
@@ -1,46 +1,60 @@
 import './chatOnline.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import React from 'react'
+export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
+  const [friends, setFriends] = useState([])
+  const [onlineFriends, setOnlineFriends] = useState([])
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER
 
-export default function ChatOnline() {
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios('/users/friends/' + currentId)
+      setFriends(res.data)
+    }
+
+    getFriends()
+  }, [currentId])
+
+  useEffect(() => {
+    setOnlineFriends(friends.filter(f => onlineUsers.includes(f._id)))
+  }, [friends, onlineUsers])
+
+  const handleClick = async user => {
+    try {
+      const res = await axios.get(
+        `/conversations/find/${currentId}/${user._id}`
+      )
+      setCurrentChat(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="chatOnline">
-      <div className="chatOnlineFriend">
-        <div className="chatOnlineImgContainer">
-          <img
-            className="chatOnlineImg"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP86IyvYRW2t08mwk3yTBVavGnSDTOmW0IlBF5O47Tb7lkBMI05zqBrdb1sKjmLx5az9M&usqp=CAU"
-            alt=""
-            className="conversationImg"
-          />
-          <div className="chatOnlineBadge"></div>
+      {onlineFriends.map(o => (
+        <div
+          className="chatOnlineFriend"
+          onClick={() => {
+            handleClick(o)
+          }}
+        >
+          <div className="chatOnlineImgContainer">
+            <img
+              className="chatOnlineImg"
+              src={
+                o.profilePicture
+                  ? PF + o.profilePicture
+                  : PF + 'person/noavatar.png'
+              }
+              alt=""
+            />
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlineName">{o.username}</span>
         </div>
-        <span className="chatOnlineName">John Doe</span>
-      </div>
-      <div className="chatOnlineFriend">
-        <div className="chatOnlineImgContainer">
-          <img
-            className="chatOnlineImg"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP86IyvYRW2t08mwk3yTBVavGnSDTOmW0IlBF5O47Tb7lkBMI05zqBrdb1sKjmLx5az9M&usqp=CAU"
-            alt=""
-            className="conversationImg"
-          />
-          <div className="chatOnlineBadge"></div>
-        </div>
-        <span className="chatOnlineName">John Doe</span>
-      </div>
-      <div className="chatOnlineFriend">
-        <div className="chatOnlineImgContainer">
-          <img
-            className="chatOnlineImg"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP86IyvYRW2t08mwk3yTBVavGnSDTOmW0IlBF5O47Tb7lkBMI05zqBrdb1sKjmLx5az9M&usqp=CAU"
-            alt=""
-            className="conversationImg"
-          />
-          <div className="chatOnlineBadge"></div>
-        </div>
-        <span className="chatOnlineName">John Doe</span>
-      </div>
+      ))}
     </div>
   )
 }
